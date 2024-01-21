7b9caf5d618b5e263d85f985376e486af72e5229
Matej Vrecar <matej.vrecar.2020@uni.strath.ac.uk>
2024-01-21 15:39:54 +0000

    Even bigger advances with the GUI :))

--- src/Controller.py
+++ src/Controller.py
@@ -24,9 +24,16 @@ class Controller():
         self.analisys = Analisys()
 
         self.basic_list_of_strategies = [self.s1, self.s2, self.s3, self.s4, self.s5, self.s6, self.s7, self.s8, self.s9]
+        self.custom_list_of_strategies = []
 
-    def create_basic_tournament(self):
-        self.tournament = Tournament(self.basic_list_of_strategies)
+    def fill_with_basic_strategies(self):
+        self.custom_list_of_strategies = self.basic_list_of_strategies
+
+    def create_tournament(self, number_of_iterations):
+        self.tournament = Tournament(self.custom_list_of_strategies, iterations=number_of_iterations)
+
+    def clear_custom_list_of_strategies(self):
+        self.custom_list_of_strategies = []
 
     def play_tournament(self):
         self.tournament.play_basic_tournament()
@@ -35,6 +42,10 @@ class Controller():
 
     def table_of_averages(self):
         self.analisys.create_table_of_averages()
+
+    def clear(self):
+        self.tournament = None
+        self.analisys = Analisys()
 
 """
 print(tournament.tournament_history)
--- src/GUI.py
+++ src/GUI.py
@@ -11,19 +11,36 @@ class StrategyAdder(customtkinter.CTkToplevel):
 class ManagementFrame(customtkinter.CTkFrame):
         def __init__(self, master):
             super().__init__(master)
-            self.addButton = customtkinter.CTkButton(self, text="Add strategy", command=self.open_strategy_adder)
-            self.addButton.grid(row=0, column=0, padx=10, pady=10)
-            self.createTournamentButton = customtkinter.CTkButton(self, text="Create tournament")
-            self.createTournamentButton.grid(row=1, column=0, padx=10, pady=10)
-            self.clearButton = customtkinter.CTkButton(self, text="Clear")
-            self.clearButton.grid(row=2, column=0, padx=10, pady=10)
-            self.strategy_adder = None
+            
+            #fill with basic strategies
+            self.fill_with_basic_strategies_button = customtkinter.CTkButton(self, text="Fill with basic strategies", command= lambda : self.fill_with_basic_strategies(master))
+            self.fill_with_basic_strategies_button.grid(row=0, column=0, padx=10, pady=10,)
 
-        def open_strategy_adder(self):
-            if self.strategy_adder == None or not self.strategy_adder.winfo_exists():
-                self.strategy_adder = StrategyAdder(self)
+            #create tournament from filled strategies
+            self.create_tournament_button = customtkinter.CTkButton(self, text="Create tournament", command= lambda : self.create_tournament(master))
+            self.create_tournament_button.grid(row=1, column=0, padx=10, pady=10)
+
+            #run tournament
+            self.run_tournament_button = customtkinter.CTkButton(self, text="Run Tournament", command= lambda : self.run_tournament(master))
+            self.run_tournament_button.grid(row=2, column=0, padx=10, pady=10,)
+
+        def fill_with_basic_strategies(self, master):
+            master.controller.fill_with_basic_strategies()
+            master.custom_management_frame.update(master)
+
+        def create_tournament(self, master):
+            if master.controller.custom_list_of_strategies != []:
+                master.controller.create_tournament(200)
+                master.update_main_textbox("Tournament was created!")
             else:
-                self.strategy_adder.focus()
+                master.update_main_textbox("Not enough strategies for a tournament, please add more!")
+
+        def run_tournament(self, master):
+            if master.controller.tournament != None:
+                master.controller.play_tournament()
+                master.update_main_textbox("Ran tournament")
+            else:
+                master.update_main_textbox("Tournament not created yet! Please create the tournament first!")
 
 class AnalisysFrame(customtkinter.CTkFrame):
 
@@ -31,22 +48,20 @@ class AnalisysFrame(customtkinter.CTkFrame):
         super().__init__(master)
 
         #table of averages button
-        self.display_table_of_averages = customtkinter.CTkButton(self, text="Table of averages", command= lambda : self.show_table_of_averages(master))
-        self.display_table_of_averages.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.display_table_of_averages_button = customtkinter.CTkButton(self, text="Table of averages", command= lambda : self.show_table_of_averages(master))
+        self.display_table_of_averages_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
 
         #history table button
-        self.display_history_table = customtkinter.CTkButton(self, text="History table", command= lambda : self.show_history_table(master))
-        self.display_history_table.grid(row=0, column=1, padx=10, pady=10, sticky="e")
+        self.display_history_table_button = customtkinter.CTkButton(self, text="History table", command= lambda : self.show_history_table(master))
+        self.display_history_table_button.grid(row=0, column=1, padx=10, pady=10, sticky="e")
 
         #strategy history table input box
         self.strategy_input_box = customtkinter.CTkEntry(self, placeholder_text="Strategy name")
         self.strategy_input_box.grid(row=1, column=0)
 
         #strategy history table button
-        self.display_strategy_history_table = customtkinter.CTkButton(self, text="Strategy table", command= lambda : self.show_strategy_history_table(master))
-        self.display_strategy_history_table.grid(row=1, column=1, padx=10, pady=10, sticky="w")
-
-
+        self.display_strategy_history_table_button = customtkinter.CTkButton(self, text="Strategy table", command= lambda : self.show_strategy_history_table(master))
+        self.display_strategy_history_table_button.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
     def show_table_of_averages(self, master):
         table = master.controller.analisys.create_table_of_averages()
@@ -62,27 +77,43 @@ class AnalisysFrame(customtkinter.CTkFrame):
         master.update_main_textbox(table)
         
 
+class CustomManagementFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+
+    def update(self, master):
+        for i, strategy in enumerate(master.controller.custom_list_of_strategies):
+            self.label = customtkinter.CTkLabel(self, text=strategy.name())
+            self.label.grid(row=i//2, column=(i+1)%2, pady=2, padx=10)
+            
 
 class App(customtkinter.CTk):
     
     def __init__(self):
         super().__init__()
         self.controller = Controller()
-        self.controller.create_basic_tournament()
-        self.controller.play_tournament()
         self.title("Iterated Prisoners Dilemma Tournament")
         self.geometry("1200x800")
         self.resizable(False, False)
-        self.main_textbox = customtkinter.CTkTextbox(self, height=780, width=580, state="disabled", font=customtkinter.CTkFont("terminal", 14))
-        self.main_textbox.grid(row=0, column=1, padx=10, pady=10, sticky="e")
+        self.main_textbox = customtkinter.CTkTextbox(self, state="disabled", font=customtkinter.CTkFont("terminal", 14))
+        self.main_textbox.grid(row=0, column=1, rowspan=3, padx=10, pady=10, sticky="nesw")
         self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)  
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
-        self.analisys_frame = AnalisysFrame(self)
-        self.analisys_frame.grid(row=0, column=0)
+        self.management_frame = ManagementFrame(self)
+        self.management_frame.grid(row=0, column=0)
+
+        self.custom_management_frame = CustomManagementFrame(self)
+        self.custom_management_frame.grid(row=1, column=0, sticky="ew")
         
-        #self.management_frame = ManagementFrame(self)
-        #self.management_frame.grid(row=0, column=0)
+        self.analisys_frame = AnalisysFrame(self)
+        self.analisys_frame.grid(row=2, column=0)
+        
+        
 
     def update_main_textbox(self, value):
         self.main_textbox.configure(state="normal")
